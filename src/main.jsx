e665cc44a1cb941e755d9ee8df3cdb2be88f1424
poojatank15 <poojatank23015@gmail.com>
2025-08-08 11:07:15 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
 import Cardview from './Cardview.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+{/*     <App /> */}
     <Cardview/>
   </StrictMode>,
 )
